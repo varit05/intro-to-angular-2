9b5cc9adf10376d48e28a8a7ac2580ba2358fc32
Varit Patel <varitpatel@gmail.com>
2016-06-28 22:36:09 +0530

    Lesson04-events-and-refs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
 //import "./lessons/01-hello-world/main";
 //import "./lessons/02-first-component/main";
-import "./lessons/03-attribute-directive/main";
+//import "./lessons/03-attribute-directive/main";
+import "./lessons/04-events-and-refs/main";
---
+++ src/lessons/04-events-and-refs/main.ts
@@ -0,0 +1,13 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {Component} from 'angular2/core';
+import {TodoInput} from './todo-input';
+
+@Component({
+	selector: 'app',
+	directives: [TodoInput],
+	template: '<div><todo-input></todo-input></div>'
+})
+
+class App{}
+
+bootstrap(App);
